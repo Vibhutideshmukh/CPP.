9fb26f692e1994e2224c6bb0a145fff874983fc9
Vibhutideshmukh <161445282+Vibhutideshmukh@users.noreply.github.com>
2024-10-25 23:19:42 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,29 @@
+2. Swap two numbers using parameterized constructor.
+   
+ #include <iostream>
+using namespace std;
+
+class A{
+    public:
+        int a;
+        int b;
+        int temp;
+    
+    A(int x,int y)
+    {
+        a=x;
+        b=y;
+        
+        cout<<"Before swap:"<<a<<" "<<b<<endl;
+    
+    temp=a;
+    a=b;
+    b=temp;
+    
+    cout<<"after swap:"<<a<<" "<<b<<endl;
+    }
+};
+int main()
+{
+    A ob(5,6);
+}
